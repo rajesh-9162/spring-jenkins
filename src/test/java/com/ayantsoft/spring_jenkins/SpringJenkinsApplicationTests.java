6d05359e9665ce91933613c607c6d10ab26ccb92
Rajesh <rajgorain.339@gmail.com>
2026-04-22 19:55:23 +0530

    first commit

---
+++ src/test/java/com/ayantsoft/spring_jenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ayantsoft.spring_jenkins;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(true, true);
+	}
+
+}
